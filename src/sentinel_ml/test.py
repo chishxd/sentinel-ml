b249b6e3dd6a6a60fe10168bf258efda2dbd76fb
Chinmay Shet <chishxd@gmail.com>
2025-07-11 14:21:30 +0530

    Added main.py, started working on FastAPI api

--- src/sentinel_ml/test.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from pathlib import Path
-
-current_file = Path(__file__).resolve()
-project_root = current_file.parents[2]
-df = pd.read_csv(f"{project_root}/data/HDFS_100k.log_structured.csv")
-
-print(df.head())
